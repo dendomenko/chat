9aa6308745bb4dcc58cba6866e03707522de444c
dendomenko <dendomenko@gmail.com>
2016-12-13 01:15:36 +0200

    Add WebSockets to env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.allowed_request_origins = ['https://agile-escarpment-94483.herokuapp.com/', 'http://agile-escarpment-94483.herokuapp.com/']
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -83,4 +85,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+ 
 end
