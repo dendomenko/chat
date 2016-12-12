a49bb9b75367af5f063cef3871784ee3aec4ac2f
dendomenko <dendomenko@gmail.com>
2016-12-13 01:19:37 +0200

    Fix production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
-  config.action_cable.allowed_request_origins = ['https://agile-escarpment-94483.herokuapp.com/', 'http://agile-escarpment-94483.herokuapp.com/']
+  config.web_socket_server_url = "wss://agile-escarpment-94483.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://agile-escarpment-94483.herokuapp.com', 'http://agile-escarpment-94483.herokuapp.com']
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
